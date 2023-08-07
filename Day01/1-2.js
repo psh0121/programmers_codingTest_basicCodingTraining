767a606d561e6a4cb4636f5476b2630d9953b0ec
psh0121 <tngus2057@gmail.com>
2023-08-07 10:18:09 +0900

    [Day01] a와 b 출력하기 입출력 내용 추가

--- Day01/1-2.js
+++ Day01/1-2.js
@@ -3,6 +3,9 @@
 // 정수 a와 b가 주어집니다. 
 // 각 수를 입력받아 입출력 예와 같은 형식으로 출력하는 코드를 작성해 보세요.
 
+// 입력: 4 5
+// 출력: a = 4 \n b = 5
+
 const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
