145ec4ac3eecf696aa5bc0f1ed1a204973f2db2e
psh0121 <tngus2057@gmail.com>
2023-08-13 17:55:18 +0900

    [Day03] 문자열 곱하기

---
+++ Day03/3-3.js
@@ -0,0 +1,13 @@
+// 문자열 곱하기
+
+// 문자열 my_string과 정수 k가 주어질 때, 
+// my_string을 k번 반복한 문자열을 return 하는 solution 함수를 작성해 주세요.
+
+// 입력: "string", 3
+// 출력: "stringstringstring"
+
+function solution(my_string, k) {
+    return my_string.repeat(k);
+}
+
+console.log(solution("string", 3));
