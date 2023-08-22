cac771b9b23d54e3284a995c402acdb6892faa33
psh0121 <tngus2057@gmail.com>
2023-08-22 13:24:19 +0900

    [Day14] n보다 커질 때까지 더하기

---
+++ Day14/14-4.js
@@ -0,0 +1,20 @@
+// n보다 커질 때까지 더하기
+
+// 정수 배열 numbers와 정수 n이 매개변수로 주어집니다. 
+// numbers의 원소를 앞에서부터 하나씩 더하다가 그 합이 n보다 커지는 순간 이때까지 더했던 원소들의 합을 return 하는 solution 함수를 작성해 주세요.
+
+// 입력: [34, 5, 71, 29, 100, 34], 123
+// 출력: 139
+
+function solution(numbers, n) {
+    let result = 0;
+
+    for(let i = 0; i < numbers.length; i++) {
+        if(result > n) break;
+        result += numbers[i];
+    }
+
+    return result;
+}
+
+console.log(solution([34, 5, 71, 29, 100, 34], 123));
