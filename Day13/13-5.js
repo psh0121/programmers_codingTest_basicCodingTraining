51f9c41fd1a93b255ce8ed87f202fe161247ac3b
psh0121 <tngus2057@gmail.com>
2023-08-22 13:05:41 +0900

    [Day13] n개 간격의 원소들

---
+++ Day13/13-5.js
@@ -0,0 +1,12 @@
+// n개 간격의 원소들
+
+// 정수 리스트 num_list와 정수 n이 주어질 때, num_list의 첫 번째 원소부터 마지막 원소까지 n개 간격으로 저장되어있는 원소들을 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+
+// 입력: [4, 2, 6, 1, 7, 6], 2
+// 출력: [4, 6, 7]
+
+function solution(num_list, n) {
+    return num_list.filter((el, i) => i % n === 0);
+}
+
+console.log(solution([4, 2, 6, 1, 7, 6], 2));
