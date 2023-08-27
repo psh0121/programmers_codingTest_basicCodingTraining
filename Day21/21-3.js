fb9cfeac7670a3cc8f243c747c2f591af1dac031
psh0121 <tngus2057@gmail.com>
2023-08-27 23:15:00 +0000

    [Day21] 정수 부분

---
+++ Day21/21-3.js
@@ -0,0 +1,12 @@
+// 정수 부분
+
+// 실수 flo가 매개 변수로 주어질 때, flo의 정수 부분을 return하도록 solution 함수를 완성해주세요.
+
+// 입력: 1.42
+// 출력: 1
+
+function solution(flo) {
+    return Math.floor(flo);
+}
+
+console.log(solution(1.42));
