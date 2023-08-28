829045f9cd33a96f9d6c2259b14b9461baedf0b0
psh0121 <tngus2057@gmail.com>
2023-08-28 12:52:29 +0900

    [Day22] 문자열로 변환

---
+++ Day22/22-3.js
@@ -0,0 +1,12 @@
+// 문자열로 변환
+
+// 정수 n이 주어질 때, n을 문자열로 변환하여 return하도록 solution 함수를 완성해주세요.
+
+// 입력: 123
+// 출력: "123"
+
+function solution(n) {
+    return String(n);
+}
+
+console.log(solution(123));
