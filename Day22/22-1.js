b9fc77fc751462c3eddb4890fb192a53156d422e
psh0121 <tngus2057@gmail.com>
2023-08-28 12:46:35 +0900

    [Day22] 0 떼기

---
+++ Day22/22-1.js
@@ -0,0 +1,12 @@
+// 0 떼기
+
+// 정수로 이루어진 문자열 n_str이 주어질 때, n_str의 가장 왼쪽에 처음으로 등장하는 0들을 뗀 문자열을 return하도록 solution 함수를 완성해주세요.
+
+// 입력: "0010"
+// 출력: "10"
+
+function solution(n_str) {
+    return String(+n_str);
+}
+
+console.log(solution("0010"));
